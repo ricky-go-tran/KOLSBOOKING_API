a3d4d9459eb705bd378a5afc47aa10b5d8a81cd3
Ricky <ricky.tran.goldenowl@gmail.com>
2023-10-10 20:40:23 +0700

    Fix serect key

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::WebhooksController < ApplicationController
     payload = request.body.read
     sig_header = request.env['HTTP_STRIPE_SIGNATURE']
     event = nil
-    endpoint_secret = 'whsec_212bba1985bbf1eca0a713e22a3d91843a0ffbc62b9fe538c836658586b42db9'
+    endpoint_secret = Rails.application.credentials.webhook_key
     begin
       event = Stripe::Webhook.construct_event(
         payload, sig_header, endpoint_secret
